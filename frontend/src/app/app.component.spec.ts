0e61e6b3dd494047090294dc3b05ebc5c8001b46
sizukutamago <sizukutamago@gmail.com>
2022-07-24 18:39:48 +0900

    add player module

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -24,8 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'frontend app is running!',
+    expect(compiled.querySelector('header span')?.textContent).toContain(
+      'player',
     );
   });
 });
