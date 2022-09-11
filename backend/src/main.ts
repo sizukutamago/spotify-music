090ba8299c352aec789ae3145a8c5a346c2c1b2f
sizukutamago <sizukutamago@gmail.com>
2022-09-11 18:21:46 +0900

    spdy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as fs from 'fs';
+import * as spdy from 'spdy';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const expressApp = express();
+  const spdyOpts = {
+    key: fs.readFileSync('./src/test.key'),
+    cert: fs.readFileSync('./src/test.crt'),
+  };
+  const server = spdy.createServer(spdyOpts, expressApp);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
   app.enableCors();
-  await app.listen(3000);
+  await app.init();
+  await server.listen(3000);
 }
 bootstrap();
